4b5849b6514eaa1aeaaa8353e137faed4e524fb2
Dixon Adair <dixonadairv@gmail.com>
2015-08-29 17:55:47 -0700

    try again

--- app/assets/javascripts/versions/with_polyline_buffer.js
+++ app/assets/javascripts/versions/with_polyline_buffer.js
@@ -7,6 +7,8 @@ $(function() {
 
 	// set some "debugging variables" (when a variable is set to true, it means that more information than would normally appear on the final user-facing site will appear, which helps me with debugging)
 
+	// for some reason on heroku, it currently says that getCurrentPosition function has been deprecated
+	var getCurrentPositionWorks = false;
 	var useSampleAddresses = true;
 	var showPolylineBufferOnMap = false;
 	var showRectangleAroundPolylineBuffer = false;
@@ -80,19 +82,12 @@ $(function() {
 	var directionsService = new google.maps.DirectionsService();
 	var service, infoWindow, map;
 
-	function getGeolocation() {
-		return new Promise(function(resolve, reject) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-				resolve(position);
-			});
-		});
-	};
-
 	var centerSF = new google.maps.LatLng(37.766280, -122.420961);
 	var centerATL = new google.maps.LatLng(33.8, -84.3);
 	var mapOptions = {
     	zoom: 11,
-    	mapTypeId: google.maps.MapTypeId.ROADMAP
+    	mapTypeId: google.maps.MapTypeId.ROADMAP,
+    	center: centerSF
 	}
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	infoWindow = new google.maps.InfoWindow();
@@ -100,15 +95,25 @@ $(function() {
 	directionsDisplay.setMap(map);
 
 	// Geolocation
-	var compGeolocation, mapCenter;
-	if (!!navigator.geolocation) {
-		compGeolocation = getGeolocation()
-		compGeolocation.then(function(position) {
-			mapCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			map.setCenter(mapCenter);
-		});
-	} else {
-		console.log('Geolocation not available');
+	if (getCurrentPositionWorks === true) {
+		function getGeolocation() {
+			return new Promise(function(resolve, reject) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					resolve(position);
+				});
+			});
+		};
+
+		var compGeolocation, mapCenter;
+		if (!!navigator.geolocation) {
+			compGeolocation = getGeolocation()
+			compGeolocation.then(function(position) {
+				mapCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				map.setCenter(mapCenter);
+			});
+		} else {
+			console.log('Geolocation not available');
+		};
 	};
 
 	// ====================================================
